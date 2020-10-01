0cceb0f29b989d30afd788a71fb1c8947a2f54a0
onndr <onndr@users.noreply.github.com>
2020-10-01 16:47:54 +0300

    Updates

---
+++ precache-manifest.ca47ff04a6f8e69db23ba41e1488461c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673c52ca93927128ecd3aaf5675be059",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "7b8a5b0148230f00abc0",
+    "url": "/weather/static/css/main.d2a37244.chunk.css"
+  },
+  {
+    "revision": "dce161451e243ef982db",
+    "url": "/weather/static/js/2.e16f023b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.e16f023b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b8a5b0148230f00abc0",
+    "url": "/weather/static/js/main.33352594.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  },
+  {
+    "revision": "3741b811a922a1aa6b5d67531bd77580",
+    "url": "/weather/static/media/preloading-img.3741b811.svg"
+  }
+]);
